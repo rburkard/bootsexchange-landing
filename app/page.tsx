ff3cfc81f5dd7523e07867da0aa57041836c2298
Roman Burkard <roman.burkard@gmail.com>
2023-09-25 16:49:06 +0200

    fix scroll

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import {Button} from './components/button';
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col justify-center items-center p-10 pt-24 gap-16 ">
+		<main className="flex flex-col justify-center items-center p-10 pt-24 gap-16 ">
 			{/* Header */}
 			<div className="z-10 gap-5 fixed left-0 top-0 flex w-full justify-center align-middle border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit">
 				<Image alt={'logo'} src={'/logo.png'} width={40} height={40} />
@@ -27,8 +27,6 @@ export default function Home() {
 				<Button title={'Verkaufen'} />
 				<Button title={'Kaufen und Verkaufen'} />
 			</div>
-
-			<div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"></div>
 		</main>
 	);
 }
